eb82dd3c9d2aba6f6d9fe029aab3d0ed236b6eba
Wen Li <wenlilearn@gmail.com>
2020-08-12 10:27:05 -0400

    Block is done

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block {
   constructor(timestamp, lastHash, hash, data) {
     this.timestamp = timestamp;
@@ -14,6 +16,23 @@ class Block {
       Data: ${this.data}
     `;
   }
+
+  static genesis() {
+    return new this("Genesis time", "-----", "f1r57-h45h", []);
+  }
+
+  static mineBlock(lastBlock, data) {
+    // Epoch time
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
 }
 
-export default Block;
+module.exports = Block;
